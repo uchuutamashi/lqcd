b73f218593cecf0313e3d97b7ce2262844baab2e
ytc <ytc@mit.edu>
2015-06-10 13:32:14 -0400

    update

--- su3/tension.m
+++ su3/tension.m
@@ -1,11 +1,14 @@
 load out.txt
 k=5; %cutoff
-sigma=ones(1,20);
-for numrun = 1:20
+n=8; %lattice size
+N=20;%num ofconfigurations
+
+sigma=ones(1,N);
+for numrun = 1:N
    x=1:k;
    v=ones(1,k);
    for r=1:k
-      y=out( (numrun-1)*8+r , 1:k );
+      y=out( (numrun-1)*n+r , 1:k );
       fm=fit(x',y','exp1');
       v(r)=abs(fm.b);
    end
