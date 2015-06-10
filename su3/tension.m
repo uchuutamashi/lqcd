4a047200c322be54dbdba9530fd66e6d1638d37a
ytc <ytc@mit.edu>
2015-06-10 13:30:12 -0400

    +MATLAB script for tension

--- su3/tension.m
+++ su3/tension.m
@@ -1,10 +1,11 @@
 load out.txt
+k=5; %cutoff
 sigma=ones(1,20);
 for numrun = 1:20
-   x=1:5;
-   v=ones(1,5);
-   for r=1:5
-      y=out( (numrun-1)*8+r , 1:5 );
+   x=1:k;
+   v=ones(1,k);
+   for r=1:k
+      y=out( (numrun-1)*8+r , 1:k );
       fm=fit(x',y','exp1');
       v(r)=abs(fm.b);
    end
